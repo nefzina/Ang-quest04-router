b234b236be887d307e3054a03bd9ec96950bcc69
nefzina <nefzi.amani@gmail.com>
2023-09-21 09:21:19 +0200

    creating routes for home & contact manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import {RouterModule, RouterLink} from '@angular/router';
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
